b5f1019d8cb686073934d714f74d6978f317196d
Niels <info@heimseiten.de>
2021-08-09 17:26:33 +0200

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -8,10 +8,16 @@ use Contao\System;
 $GLOBALS['TL_DCA']['tl_content']['fields']['textImagePositioning'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_content']['textImagePositioning'],
     'inputType' => 'select',
-    'eval'      => array('tl_class'=>'w50'),
+    'eval'      => array('tl_class'=>'w50 clr'),
     'options'   => array('', 'imageBesideTextCentered', 'fullWidhElementTextBesideCroppedImage', 'textAbsoluteAtImageBottom'),
     'reference' => &$GLOBALS['TL_LANG']['textImagePositioning'], 
     'sql'       => "varchar(255) NOT NULL default ''"
+];
+$GLOBALS['TL_DCA']['tl_content']['fields']['centerImage'] = [
+    'label' => &$GLOBALS['TL_LANG']['tl_content']['centerImage'],
+    'inputType' => 'checkbox', 
+    'eval'      => array('tl_class' => 'w50 m12'),
+    'sql'       => "char(1) NOT NULL default ''" 
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = function()
@@ -22,6 +28,7 @@ $GLOBALS['TL_DCA']['tl_content']['config']['onload_callback'][] = function()
         {   
             if ($key == 'text') { 
                 PaletteManipulator::create()
+                ->addField('centerImage', 'fullsize')
                 ->addField('textImagePositioning', 'fullsize')
                 ->applyToSubpalette('addImage', 'tl_content');
             } else {
