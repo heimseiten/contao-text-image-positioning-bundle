e0cab209d32802c8b2d320abc12a1b48ab02b340
Niels <info@heimseiten.de>
2022-08-19 17:02:00 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,8 +1,3 @@
 <?php
 
-use Contao\System;
-use Symfony\Component\HttpFoundation\Request;
-
-if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaotextimagepositioning/contao_text_image_positioning_bundle.scss|static';
-}
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaotextimagepositioning/contao_text_image_positioning_bundle.scss|static';
