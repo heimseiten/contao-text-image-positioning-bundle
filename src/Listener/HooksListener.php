62c44759bf48510c30fb0a21c4a4a624845aa57f
Niels <info@heimseiten.de>
2025-12-17 13:38:41 +0100

    Update class names in HooksListener.php

--- src/Listener/HooksListener.php
+++ src/Listener/HooksListener.php
@@ -27,22 +27,22 @@ class HooksListener {
         if ( $object->addImage ) {
 
             if ( $object->textImagePositioning == 'imageBesideTextCentered') {
-                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 text_in_center_of_image"', $buffer, 1);
+                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 TextMittig"', $buffer, 1);
             }
             if ( $object->textImagePositioning == 'textBesideCroppedFullWidhImage') {
-                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 image_full_width_height_as_text"', $buffer, 1);
+                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 BildBisZumRand"', $buffer, 1);
             }
             if ( $object->textImagePositioning == 'textBesideCroppedImage') {
-                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 image_height_as_text"', $buffer, 1);
+                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 BildBisZurHälfte"', $buffer, 1);
             }   
             if ( $object->textImagePositioning == 'fullWidthImageBackground') {
-                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 image_as_background"', $buffer, 1);
+                $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 BildHinterText"', $buffer, 1);
             }
         
         }
                
         if ( $object->textImagePositioning == 'centerImage') {
-            $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 image_horizontal_centered"', $buffer, 1);
+            $buffer = preg_replace('/class="([^"]+)"/', 'class="$1 BildMittig"', $buffer, 1);
         }
         if ( $object->imageCssFilter ) {
             $buffer = preg_replace('/<img/', '<img style="filter: ' . $object->imageCssFilter . ';"', $buffer, 1);
