310f51ef1875dfa555c1f33f6de126008fd42c3c
Niels <info@heimseiten.de>
2026-06-23 10:32:23 +0200

    Modernize bundle layout (src/Resources → project root, AbstractBundle)

--- src/HeimseitenContaoTextImagePositioningBundle.php
+++ src/HeimseitenContaoTextImagePositioningBundle.php
@@ -2,32 +2,16 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Contao Text-Bild Positionierungen.
- * 
- * (c) heimseiten.de - Webdesign aus Köln 2021 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-text-image-positioning-bundle
- */
-
 namespace Heimseiten\ContaoTextImagePositioningBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-/**
- * Class HeimseitenContaoTextImagePositioningBundle
- */
-class HeimseitenContaoTextImagePositioningBundle extends Bundle
+class HeimseitenContaoTextImagePositioningBundle extends AbstractBundle
 {
-	/**
-	 * {@inheritdoc}
-	 */
-	public function build(ContainerBuilder $container): void
-	{
-		parent::build($container);
-		
-	}
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yml');
+    }
 }
